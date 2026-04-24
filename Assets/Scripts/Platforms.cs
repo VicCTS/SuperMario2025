662f559818cfcaf232f98dc7808ca01bb04992ee
VicCTS <victordm.cts@gmail.com>
2026-04-24 11:45:36 +0200

    24/04/2026

---
+++ Assets/Scripts/Platforms.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Platforms : MonoBehaviour
+{
+    private BoxCollider2D _boxCollider;
+
+    private InputAction _moveAction;
+    private Vector2 _moveInput;
+
+    [SerializeField]private bool isOnPlatform;
+
+
+    void Awake()
+    {
+        _boxCollider = GetComponent<BoxCollider2D>();
+        _moveAction = InputSystem.actions["Move"];
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _moveInput = _moveAction.ReadValue<Vector2>();
+
+        Debug.Log(_moveInput);
+
+        if(_moveInput.y < 0 && isOnPlatform)
+        {
+            _boxCollider.isTrigger = true;
+            isOnPlatform = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            isOnPlatform = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            isOnPlatform = false;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if(collider.gameObject.CompareTag("Player"))
+        {
+            _boxCollider.isTrigger = false;
+        }
+    }
+}
